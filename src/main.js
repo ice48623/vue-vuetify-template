777f9f1c2a4d057db2ebaca9d7b8f684feef83b2
ice48623 <ice48623@gmail.com>
2019-12-12 15:36:51 +0700

    update dependencies

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
 import Vue from 'vue'
 import './plugins/axios'
-import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import vuetify from './plugins/vuetify';
 import { store } from './store'
 
-Vue.use(Vuetify, {
-    iconfont: 'md',
-});
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App)
